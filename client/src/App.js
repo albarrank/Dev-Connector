51464229f7b5603f4e9b6d7904fe54f064718592
Jack Frost <albarrankevin97@gmail.com>
2019-08-21 21:19:10 -0700

    added edit profile button for users viewing their own profile for developers

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
 import Dashboard from './components/dashboard/Dashboard';
 import Profiles from './components/profiles/Profiles';
+import Profile from './components/profile/Profile';
 
 // Forms
 import AddExperience from './components/profile-forms/AddExperience';
@@ -49,6 +50,7 @@ const App = () => {
                 <Route exact path='/register' component={Register} />
                 <Route exact path='/login' component={Login} />
                 <Route exact path='/profiles' component={Profiles} />
+                <Route exact path='/profile/:id' component={Profile} />
                 <PrivateRoute exact path='/dashboard' component={Dashboard} />
                 <PrivateRoute exact path='/edit-profile' component={EditProfile} />
                 <PrivateRoute exact path='/create-profile' component={CreateProfile} />
--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -1,18 +1,36 @@
 import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import { getProfileById } from '../../actions/profile';
-const Profile = ({ match, getProfileById, profile: { profile, loading}, auth }) => {
+const Profile = ({ 
+    match,
+    getProfileById, 
+    profile: { profile, loading}, 
+    auth 
+}) => {
 
     useEffect(() => {
         getProfileById(match.params.id);
     }, [getProfileById]);
 
     return (
-        <div>
-            profile
-        </div>
+        <Fragment>
+            {
+                profile === null || loading ? 
+                <Spinner/> :
+                <Fragment>
+                    <Link to='/profiles' className='btn btn-primary'>
+                        Back To Profiles
+                    </Link>
+                    {
+                        auth.isAuthenticated && auth.loading === false && auth.user._id === profile.user._id 
+                        && (<Link to='/edit-profile' className='btn btn-dark'>Edit</Link>)}
+                </Fragment> 
+                
+            }
+        </Fragment>
     )
 };
 
