e0579e35cf0b7d650cd5535c7148a2647cd35c6b
Jack Frost <albarrankevin97@gmail.com>
2019-08-29 21:33:22 -0700

    changed heroku configuration to connect to github repo

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
-const path = require('path');
+// const path = require('path');
 
 const app = express();
 
@@ -24,9 +24,9 @@ if (process.env.NODE_ENV === "production") {
     // Set static folder
     app.use(express.static('client/build'));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
+    // app.get('*', (req, res) => {
+    //     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    // });
 }
 
 app.listen(PORT, () => {
